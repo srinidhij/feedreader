76f405997bfe68569432ee74f35f7e8ff490ad4f
Srinidhi J <srinidhij.21@gmail.com>
2014-05-11 11:13:58 +0530

    Modularize feed manager

--- feed.py
+++ feed.py
@@ -3,71 +3,70 @@ from pymongo import MongoClient
 import simplejson as json
 from xml.etree import ElementTree
 
-def getFeedLinks():
-    
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.feedreader
-    collection = db.feedlinks
-    flc = collection.find({})
-    feedlinks = []
-    
-    for flob in flc:
-        link = {}
-        '''
-        link['link'] = flob['link'].encode('ascii','ignore')
-        link['title'] = flob['title']
-        feedlinks.append(link)
-        '''
-        print flob
-    return feedlinks
+class FeedManager:
+    def __init__(self):
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client.feedreader
+        self.collection = db.feedlinks
 
 
-
-def addFeedLink(link,title=None):
-
-    error = None    
-    if title is None:
-        title = ''
-
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.feedreader
-    collection = db.feedlinks
-
-    flc = collection.find_one({'link':link})
+    def getFeedLinks(self,):
+        flc = self.collection.find({})
+        feedlinks = []
         
-    if flc:
-        error = 'Feed already added'
+        for flob in flc:
+            link = {}
+            '''
+            link['link'] = flob['link'].encode('ascii','ignore')
+            link['title'] = flob['title']
+            feedlinks.append(link)
+            '''
+            print flob
+        return feedlinks
+
+    def addFeedLink(self,link,title=None):
+
+        error = None    
+        if title is None:
+            title = ''
+
+        collection = self.collection
+
+        flc = collection.find_one({'link':link})
+            
+        if flc:
+            error = 'Feed already added'
+            return error
+
+        fl = {
+            'link':link, 
+            'title':title
+            }
+
+        feed_id = collection.insert(fl)
+
+        if feed_id is None:
+            error = 'Failed inserting into DB'
+
         return error
 
-    fl = {
-        'link':link, 
-        'title':title
-        }
-
-    feed_id = collection.insert(fl)
-
-    if feed_id is None:
-        error = 'Failed inserting into DB'
-
-    return error
-
-def getFeeds():
-    #tree = ElementTree.parse('country_data.xml')
-    #root = ElementTree.fromstring(country_data_as_string)
-    pass
+    def getFeeds(self):
+        #tree = ElementTree.parse('country_data.xml')
+        #root = ElementTree.fromstring(country_data_as_string)
+        pass
 
 def test():
     feed = 'http://feeds.arstechnica.com/arstechnica/business?format=xml'
     title = 'Ars Technica Tech Labs'
-    
-    msg = addFeedLink(feed, title)
+    fmg = FeedManager()
+    msg = fmg.addFeedLink(feed, title)
 
     if msg is None:
         print 'Feed added Successfully'
     else:
         print 'Error: ',msg
 
-    feed_links = getFeedLinks()
+    feed_links = fmg.getFeedLinks()
     print feed_links    
 
 if __name__ == '__main__':
